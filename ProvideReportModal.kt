c05268fb5d0380566e76c3c4200c09bfeb39fae9
sryang <sarang628@gmail.com>
2025-10-30 17:36:30 +0900

    update share

--- ProvideReportModal.kt
+++ ProvideReportModal.kt
@@ -1,9 +1,16 @@
 package com.sarang.torang.di.report_di
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.sarang.torang.BuildConfig
 import com.sarang.torang.compose.bottomsheet.share.ShareModalBottomSheet
+import com.sarang.torang.compose.bottomsheet.share.components.LocalShareImageLoad
 import com.sarang.torang.compose.report.ReportModal
+import com.sarang.torang.di.bottomsheet_di.CustomShareImageLoader
 
 fun provideReportModal(): @Composable (Int, onReported: () -> Unit) -> Unit =
     { reviewId, onReported ->
@@ -14,11 +21,13 @@ fun provideReportModal(): @Composable (Int, onReported: () -> Unit) -> Unit =
         )
     }
 
-fun provideShareBottomSheetDialog(): @Composable (onClose: () -> Unit) -> Unit = {
-    ShareModalBottomSheet(
-        profileServerUrl = BuildConfig.PROFILE_IMAGE_SERVER_URL,
-        isExpand = true,
-        onSelect = {},
-        onClose = it
-    )
+fun provideShareBottomSheetDialog(show : Boolean): @Composable (onClose: () -> Unit) -> Unit = {
+    CompositionLocalProvider(LocalShareImageLoad provides CustomShareImageLoader) {
+        ShareModalBottomSheet(
+            isExpand = show,
+            reviewId = 0,
+            //onSelect = {},
+            onClose = it
+        )
+    }
 }
